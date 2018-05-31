48aed834a4a28ed9aa08ce25b318db0e8fcef9d4
feixyz10 <6270744+feixyz10@users.noreply.github.com>
2018-05-31 19:50:31 +0800

    Update deepwalk.py

--- source/deepwalk.py
+++ source/deepwalk.py
@@ -6,7 +6,7 @@ from gensim.models import Word2Vec
 class DeepWalk:
 	def __init__(self, G, emb_size=128, length_walk=40, num_walks=10, window_size=10, num_iters=1):
 		self.G = G
-        	self.emb_size = emb_size
+        self.emb_size = emb_size
 		self.length_walk = length_walk
 		self.num_walks = num_walks
 		self.window_size = window_size
